6c45fb9ec36f51106565ced5dd5d3de4c2869954
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2026-01-05 14:04:47 +0100

    Fixed an error

--- train.py
+++ train.py
@@ -70,7 +70,7 @@ def main():
     )
 
     # Trainer
-    strategy = DDPStrategy(find_unused_parameters=False)
+    strategy = DDPStrategy(find_unused_parameters=True)
     trainer = pl.Trainer(
         accelerator="gpu",
         devices=args.devices,
